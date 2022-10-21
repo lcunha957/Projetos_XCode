3193ae1d6916570cedf6f8da3ce585bb8371bb96
Lunara Cunha <88806865+lcunha957@users.noreply.github.com>
2022-10-20 22:01:04 -0300

    subindo projeto dia 20 Out
    código inicial

---
+++ projeto_20Out/aula01not/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  aula01not
+//
+//  Created by LUNARA MORENA CUNHA on 20/10/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Alô turma")
+            .padding()
+            .font(.title)
+            .foregroundColor(.green)
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ projeto_20Out/aula01not/aula01notApp.swift
@@ -0,0 +1,17 @@
+//
+//  aula01notApp.swift
+//  aula01not
+//
+//  Created by LUNARA MORENA CUNHA on 20/10/22.
+//
+
+import SwiftUI
+
+@main
+struct aula01notApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
