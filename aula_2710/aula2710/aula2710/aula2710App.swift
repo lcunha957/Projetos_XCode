c41bded01cc0ec0e745d47014dc2948d4f6030ba
Lunara Cunha <88806865+lcunha957@users.noreply.github.com>
2022-10-27 21:43:11 -0300

    aula 27-10
    código inicial

---
+++ aula_2710/aula2710/aula2710/aula2710App.swift
@@ -0,0 +1,17 @@
+//
+//  aula2710App.swift
+//  aula2710
+//
+//  Created by LUNARA MORENA CUNHA on 27/10/22.
+//
+
+import SwiftUI
+
+@main
+struct aula2710App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ aula_2710/aula2710/aula2710Tests/aula2710Tests.swift
@@ -0,0 +1,33 @@
+//
+//  aula2710Tests.swift
+//  aula2710Tests
+//
+//  Created by LUNARA MORENA CUNHA on 27/10/22.
+//
+
+import XCTest
+@testable import aula2710
+
+class aula2710Tests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
