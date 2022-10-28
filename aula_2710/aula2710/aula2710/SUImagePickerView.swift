b015e9f565e69bcf21171eaba8bb37604a02623e
Lunara Cunha <88806865+lcunha957@users.noreply.github.com>
2022-10-27 22:28:42 -0300

    colocando a classe recem criada
    .

--- aula_2710/aula2710/aula2710/SUImagePickerView.swift
+++ aula_2710/aula2710/aula2710/SUImagePickerView.swift
@@ -8,3 +8,30 @@
 import SwiftUI
 import UIKit
 
+// dá o nome da classe: criar a view e dar o update da viewcontroller, passa 3 parametros
+struct SUImagePickerVIew: UIViewControllerRepresentable{
+    
+    var sourceType: UIImagePickerController.SourceType = .photoLibrary
+    @Binding var image: Image?
+    @Binding var isPresent: Bool
+    
+    func makeCoordinator() -> ImagePickerViewCoordinator {
+        return ImagePickerViewCoordinator(image: $image, isPresent: $isPresent)
+    }
+
+    func makeUIViewController(context: Context) -> UIImagePickerController{
+        let pickerController = UIImagePickerController()
+        pickerController.sourceType = sourceType
+        pickerController.delegate = context.coordinator
+        return pickerController
+    }
+    
+    func updateUIViewController(_ uiViewController: UIImagePickerController, context: Context) {
+        // nada a fazer aqui
+    }
+}
+
+class ImagePickerViewCoordinator: NSObject, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
+    
+}
+
