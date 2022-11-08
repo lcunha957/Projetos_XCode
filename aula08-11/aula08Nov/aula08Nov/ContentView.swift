d9e764d10c4a2285a5284cbbb427d96e9398f247
Lunara Cunha <88806865+lcunha957@users.noreply.github.com>
2022-11-08 19:13:31 -0300

    código inicial 08Nov
    .

---
+++ aula08-11/aula08Nov/aula08Nov/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  aula08Nov
+//
+//  Created by LUNARA MORENA CUNHA on 08/11/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ aula08-11/aula08Nov/aula08Nov/aula08NovApp.swift
@@ -0,0 +1,17 @@
+//
+//  aula08NovApp.swift
+//  aula08Nov
+//
+//  Created by LUNARA MORENA CUNHA on 08/11/22.
+//
+
+import SwiftUI
+
+@main
+struct aula08NovApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
