b757b7734810410f794ee016f9af066857d78dfc
Lunara Cunha <88806865+lcunha957@users.noreply.github.com>
2022-11-08 19:17:22 -0300

    codigo fonte correto
    .

---
+++ aula08Nov/aula08NovStobryboard/aula08NovStobryboardTests/aula08NovStobryboardTests.swift
@@ -0,0 +1,33 @@
+//
+//  aula08NovStobryboardTests.swift
+//  aula08NovStobryboardTests
+//
+//  Created by LUNARA MORENA CUNHA on 08/11/22.
+//
+
+import XCTest
+@testable import aula08NovStobryboard
+
+class aula08NovStobryboardTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
